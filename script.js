fb3fa30ba1e6b213e9e1fc0ee4204f49f3bb0a6a
Sarahsegla <sarahsegla@icloud.com>
2023-02-07 16:19:29 +0000

    some labelling

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ var searchInput = document.getElementById('#search-input');
 
 // var forecast 
 
+// date for current day
 const bigDay = moment().format('L');
 $('.BIGday').text(bigDay);
 
-
+// dates for next 5 days with time
 const dayOne = moment().add(1, 'days').calendar();
 $('.day1').text(dayOne);
 
@@ -24,16 +25,22 @@ const dayFive = moment().add(5, 'days').calendar();
 $('.day5').text(dayFive);
 
 
+//apikey
 const APIKey = "03067ded181dcabf4f389039837fbc83";
+ 
 
 cityName = searchInput;
 
-
+// url works but not input search city
 const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" 
 + searchInput + "&appid=" + APIKey;
 
-
+// practice
 const queryURL1 = "https://api.openweathermap.org/data/2.5/weather?q=" 
 + "sheffield" + "&appid=" + APIKey;
 
 console.log(queryURL1);
+
+console.log(queryURL);
+
+
